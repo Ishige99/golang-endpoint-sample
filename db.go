102893b58768c325f8c9fa6c36c0f19e606b4a24
Osamu Ishiyama <osamu-ishiyama@two2.jp>
2023-03-20 14:02:53 +0900

    コメントの追記

--- db.go
+++ db.go
@@ -8,17 +8,23 @@ import (
 )
 
 func InitDB() {
+	// DSNの作成(DB接続情報)
 	dsn := "root:root@tcp(127.0.0.1:3307)/test_db"
+
+	// MySQLに接続
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err) // エラーがあればログに出力して終了
 	}
+
+	// 関数終了時に、DBへの接続を終了する。
+	// main関数で使用されているので、main関数の終了次第DBの接続を解除する
 	defer db.Close()
 
-	// DBに接続
+	// 実際にDBとの接続を確認
 	err = db.Ping()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(err) // エラーがあればログに出力して終了
 	} else {
 		log.Println("Success database connect.")
 	}
