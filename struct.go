0d2c5b374b9625b2da848532859a00a9abce0dee
Osamu Ishiyama <osamu-ishiyama@two2.jp>
2023-03-20 16:57:15 +0900

    レスポンスデータの修正により、構造体の変更

--- struct.go
+++ struct.go
@@ -1,7 +1,8 @@
 package main
 
 type Article struct {
-	Title   string `json:"title"`
-	Desc    string `json:"desc"`
-	Content string `json:"content"`
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Content     string `json:"content"`
 }
